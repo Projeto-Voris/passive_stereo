9e4de44fafd3c2a8cb01df20fdc60dd5d255b4b5
Gustavo Alcantara <gustavo.v.alcantara@gmail.com>
2025-01-31 10:32:14 -0300

    feat: add frame id parameter and launch arg

--- launch/triangulation_rgb.launch.py
+++ launch/triangulation_rgb.launch.py
@@ -24,7 +24,7 @@ def generate_launch_description():
         LaunchArg('yaml_file', default_value=['stereo_rgb_heavy_sim.yaml'],
                   description='YAML file where is stereoBM config'),
 
-
+        LaunchArg('frame_id', default_value=['left_camera_frame'], description='Pointcloud frame_id')
         Node(
             package='passive_stereo',
             namespace=LaunchConfig('namespace'),
@@ -40,7 +40,8 @@ def generate_launch_description():
                 ('/left/image_raw', LaunchConfig('left_image')),
                 ('/right/image_raw', LaunchConfig('right_image')),
                 ('/params', LaunchConfig('stereo_params'))
-            ]
+            ],
+            parameters=[{'frame_id':LaunchConfig('frame_id')}]
         ),
         Node(
             package='passive_stereo',
